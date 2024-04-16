6173de919d02478a0e4e833330ad48d70544e1fe
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-04-16 12:46:21 +0530

    Health Check fixed

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from dtypes import make_response
 from controllers import auth_router, customer_router
 
@@ -7,5 +7,9 @@ app.include_router(auth_router)
 app.include_router(customer_router)
 
 @app.get("/healthz")
-def health_check():
-    return make_response(200, "OK")
+def health_check(response: Response):
+    return make_response(response, 200, "OK")
+
+@app.get("/")
+def health_check(response: Response):
+    return make_response(response, 200, "OK")
